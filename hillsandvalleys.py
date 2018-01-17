ba63d3e4c0d0cf715f595a288493172559d147ed
supermax03 <maximiliano.bordon@gmail.com>
2018-01-17 14:27:14 -0300

    add author

--- hillsandvalleys.py
+++ hillsandvalleys.py
@@ -1,3 +1,6 @@
+#####################################################
+#Author: Maximiliano Bordon
+#####################################################
 def getgroundinfo(ground):
     isValley = 1
     isHill = 1
@@ -29,7 +32,8 @@ def getgroundinfo(ground):
 if __name__=='__main__':
        #ground = [0,1,0,1,0,-3,0]
        #ground=[1,0,1,1,1,0,1]
-       ground=[1,1,1,0,1]
+       #ground=[1,1,1,0,1]
+       ground=[0,0,0,0,0,1]
        hills,valleys=getgroundinfo(ground)
        print("Hills-> {0}, Valleys ->{1}".format(hills,valleys))
 
